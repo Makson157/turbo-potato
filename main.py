9e849823cd48a91373e3750baffb509ed643f649
Maxim Axenov <aksmakson@gmail.com>
2023-11-24 20:39:18 +0300

    b

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ import sys
 from PyQt5 import uic
 from random import randint
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 
 
-class Example(QMainWindow):
+class Design(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui, self')
+        self.initUt()
+
+    def initUi(self):
+        self.resize(650, 420)
+        self.btn = QPushButton('ЖМИ', self)
+        self.btn.move(50, 50)
+
+
+class Example(Design):
+    def __init__(self):
+        super().__init__()
         self.flag = False
-        self.pushButton.clicked.connect(self.go_paint)
+        self.btn.clicked.connect(self.go_paint())
 
     def go_paint(self):
         self.qp = QPainter()
         self.flag = True
         self.update()
-
-
 
     def paintEvent(self, event):
         if self.flag:
@@ -26,6 +34,7 @@ class Example(QMainWindow):
             self.qp.end()
 
     def draw_flag(self, qp):
+        r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
         qp.setBrush(QColor(255, 255, 0))
         # Рисуем прямоугольник заданной кистью
         x, y = randint(0, 316), randint(0, 267)
